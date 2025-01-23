74e9186442ccee95c2dcbdd2666e496daca50201
Kazuma Yagi <kazulab10969@outlook.jp>
2025-01-23 15:47:15 +0900

    pubisher・subscriber・action_clientを追加

--- include/waypoint_manager_cpp/waypoint_manager_cpp_component.hpp
+++ include/waypoint_manager_cpp/waypoint_manager_cpp_component.hpp
@@ -3,6 +3,10 @@
 #include <string>
 
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_msgs/msg/int32.hpp>
+#include <nav2_msgs/action/navigate_to_pose.hpp>
 
 #include "waypoint_manager_cpp/visibility_control.h"
 
@@ -15,6 +19,15 @@ public:
   explicit WaypointManagerCppComponent(const rclcpp::NodeOptions & options);
 
 private:
+  void SkipFlagCallback(const std_msgs::msg::Bool::SharedPtr msg);
+  void EventFlagCallback(const std_msgs::msg::Bool::SharedPtr msg);
+
+  rclcpp::Subscription<std_msgs::msg::Bool>::ConstSharedPtr skip_flag_sub_;
+  rclcpp::Subscription<std_msgs::msg::Bool>::ConstSharedPtr event_flag_sub_;
+  rclcpp::Publisher<std_msgs::msg::Int32>::ConstSharedPtr next_waypoint_id_pub_;
+  rclcpp::Publisher<std_msgs::msg::Int32>::ConstSharedPtr reached_waypoint_id_pub_;
+  rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::ConstSharedPtr nav2_pose_client_;
+
   // Parameters
   std::string waypoints_csv_;
   bool loop_enable_{false};
--- src/waypoint_manager_cpp_component.cpp
+++ src/waypoint_manager_cpp_component.cpp
@@ -20,10 +20,31 @@ WaypointManagerCppComponent::WaypointManagerCppComponent(const rclcpp::NodeOptio
     RCLCPP_INFO(get_logger(), "start_waypoint_id: %d", start_waypoint_id_);
     RCLCPP_INFO(get_logger(), "loop_enable: %s", loop_enable_ ? "true" : "false");
     RCLCPP_INFO(get_logger(), "loop_count: %d", loop_count_);
-    
 
+    // Subscribers
+    skip_flag_sub_ = create_subscription<std_msgs::msg::Bool>("skip_flag", 1, std::bind(&WaypointManagerCppComponent::SkipFlagCallback, this, std::placeholders::_1));
+    event_flag_sub_ = create_subscription<std_msgs::msg::Bool>("event_flag", 1, std::bind(&WaypointManagerCppComponent::EventFlagCallback, this, std::placeholders::_1));
+
+    // Publishers
+    next_waypoint_id_pub_ = create_publisher<std_msgs::msg::Int32>("next_waypoint_id", 1);
+    reached_waypoint_id_pub_ = create_publisher<std_msgs::msg::Int32>("reached_waypoint_id", 1);
+
+    // Action client for navigation
+    nav2_pose_client_ = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this, "navigatie_to_pose");
+    
     RCLCPP_INFO(this->get_logger(), "Start waypoint_manager_cpp!");
 }
+
+void WaypointManagerCppComponent::SkipFlagCallback(const std_msgs::msg::Bool::SharedPtr msg)
+{
+
+}
+
+void WaypointManagerCppComponent::EventFlagCallback(const std_msgs::msg::Bool::SharedPtr msg)
+{
+    
+}
+
 }  // namespace waypoint_manager_cpp
 
 RCLCPP_COMPONENTS_REGISTER_NODE(waypoint_manager_cpp::WaypointManagerCppComponent)
